318083423650ffa668bc45386e88c3db29ffd9d0
Jonathan Lo <kakashi169@gmail.com>
2019-04-04 19:00:59 -0700

    added card face generator, now randomly assigns cards throughout board

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
+
 $(document).ready(initializeApp)
 
 function initializeApp() {
     generateGameBoard();
+    
     $('.cardContainer').on('click', '.back', cardClicked);
-
 }
+
 var firstCardClicked = null;
 var secondCardClicked = null;
-var totalPossibleMatches = 2;
+var totalPossibleMatches = 9;
 var matchCounter = 0;
 
 var cardArray = [
-    images/babyPug.png,
-    images/layingDown.png,
-    images/leo.jpg,
-    images/potato.png,
-    images/pugSnow.png,
-    images/rainbow.jpg,
-    images/sitting.png,
-    images/sleepyPug.jpg,
-    images/whiteDog.jpg
+    'images/babyPug.png',
+    'images/layingDown.png',
+    'images/leo.jpg',
+    'images/potato.png',
+    'images/pugSnow.png',
+    'images/rainbow.jpg',
+    'images/sitting.png',
+    'images/sleepyPug.jpg',
+    'images/whiteDog.jpg'
 ];
 
 function generateGameBoard() {
+    var cardFaces = generateCardFront(cardArray);
+
     for (var rowNum = 1; rowNum <=3; rowNum++) {
         var makeRow = $('<div>').addClass('row' + rowNum);
         for (var i = 1; i <= 6; i++) {
@@ -31,14 +35,30 @@ function generateGameBoard() {
             var makeFront = $('<div>').addClass('front');
             var makeBack = $('<div>').addClass('back');
 
+            var randomCardIndex = Math.floor(Math.random() * cardFaces.length);
+            var cardFront = $('<img>').attr('src', cardFaces[randomCardIndex]);
+            cardFaces.splice(randomCardIndex, 1);
+
+            makeFront.append(cardFront);
             makeCard.append(makeFront, makeBack);
             makeCardContainer.append(makeCard);
             makeRow.append(makeCardContainer);
-           
         }
         $('.gameArea').append(makeRow)
     }
 }
+
+function generateCardFront(arr) {
+    var allCardArray = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        allCardArray.push(arr[i]);
+        allCardArray.push(arr[i]);
+    }
+    return allCardArray;
+}
+
+$('.cardContainer').on('click', '.back', cardClicked);
 
 function cardClicked() {
     // console.log(this)
@@ -46,8 +66,10 @@ function cardClicked() {
     // console.log(e)
     if ($(this).hasClass('flipped'))
     $(this).toggle().addClass('flipped');
+
     if (firstCardClicked === null) {
         return firstCardClicked = $(this).prev().find('img').attr('src');
+
     } else if (secondCardClicked === null) {
         secondCardClicked = $(this).prev().find('img').attr('src');
         if (firstCardClicked === secondCardClicked) {
